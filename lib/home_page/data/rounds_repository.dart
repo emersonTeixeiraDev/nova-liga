d70abe2e1938fa2307187265ac2e0a71d7b2814d
Emerson Teixeira <emersonteixeiraweb@gmail.com>
2023-02-01 18:49:46 -0300

    Feat image team

--- lib/home_page/data/rounds_repository.dart
+++ lib/home_page/data/rounds_repository.dart
@@ -18,7 +18,7 @@ class RoundsRepository {
       nameVisitor: 'NOV',
       numberRound: 'Rodada 1',
       localTeam: 'assets/images/mundoGaúchoMedium.png',
-      visitorTeam: 'assets/images/novaVidaMedium.png',
+      visitorTeam: 'ass',
       goalsLocalTeam: 0,
       goalsVisitorTeam: 0,
     ),
